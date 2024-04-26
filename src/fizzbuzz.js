680aaba92d78caeb42d72597b9dcb3161b6a4223
Akintayo Durotoye <tayodurotoye@gmail.com>
2024-04-26 18:12:00 +0100

    fizzbuzz function completed!

--- src/fizzbuzz.js
+++ src/fizzbuzz.js
@@ -1,4 +1,20 @@
 // TODO: Write your function in this file
-
+function twoNum(lower, upper) {
+  let arr = []
+  for (let i = lower; i <= upper; i++) {
+    if (i % 15 === 0) {
+      arr.push('FizzBuzz')
+    } else if (i % 5 === 0) {
+      arr.push('Buzz')
+    } else if (i % 3 === 0) {
+      arr.push('Fizz')
+    } else {
+        arr.push(i)
+    }
+  }
+  return arr
+}
+console.log(twoNum(2, 30))
 // TODO: Change undefined below to the name of your function
-module.exports = undefined
+module.exports = twoNum
+console.log()
